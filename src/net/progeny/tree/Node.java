e1d4c85ff058fd7ef63aa9982ea518948a8d0da1
Chris Hunt <huntca@gmail.com>
2011-07-14 09:03:29 -0700

    Added comments

--- src/net/progeny/tree/Node.java
+++ src/net/progeny/tree/Node.java
@@ -5,32 +5,64 @@ public class Node {
   private Node rightNode;
   private int value;
 
+  /**
+   * Node constructor with initial Node value
+   *
+   * @param value Initial value for this node
+   */
   public Node(int value) {
     leftNode = null;
     rightNode = null;
     this.value = value;
   }
 
+  /**
+   * Set left node pointer for this Node
+   *
+   * @param node Left node for this Node
+   */
   public void setLeftNode(Node node){
     this.leftNode = node; 
   }
+  /**
+   * Set right node pointer for this Node
+   *
+   * @param node Right node for this Node
+   */
   public void setRightNode(Node node){
     this.rightNode = node; 
   }
+  /**
+   * Set value for this Node
+   *
+   * @param value Value for this Node
+   */
   public void setValue(int value){
     this.value = value;
   }
 
+  /**
+   * @return Left node for this Node
+   */
   public Node getLeftNode() {
     return leftNode;
   }
+  /**
+   * @return Right node for this Node
+   */
   public Node getRightNode() {
     return rightNode;
   }
+  /**
+   * @return Current value for this Node
+   */
   public int getValue() {
     return value;
   }
 
+  /**
+   * @return String representation of this Node
+   */
   @Override
     public String toString() {
       String result = value + "";
--- src/net/progeny/tree/Tree.java
+++ src/net/progeny/tree/Tree.java
@@ -5,10 +5,20 @@ import net.progeny.tree.Node;
 public class Tree {
   private Node rootNode;
 
+  /**
+   * Create new Node with value and insert into this Tree
+   *
+   * @param value Initial value for new Node
+   */
   public void insertValue(int value) {
     insertNode(new Node(value));
   }
 
+  /**
+   * Insert new Node into this Tree
+   *
+   * @param newNode Node to be inserted into this Tree
+   */
   public void insertNode(Node newNode){
     if (rootNode == null) {
       rootNode = new Node(newNode.getValue());
@@ -17,6 +27,12 @@ public class Tree {
     }
   }
 
+  /**
+   * Search this tree for a value
+   *
+   * @param value Value to be searched in this Tree
+   * @return True if this tree contains value
+   */
   public boolean containsValue(int value){
     if (getNode(rootNode, value) == null) {
       return false;
@@ -24,6 +40,12 @@ public class Tree {
     return true;
   }
 
+  /** 
+   * Get Node in this Tree that contains value
+   *
+   * @param value Value to be searched within this Tree
+   * @return Node that matches value or null if no match found
+   */
   public Node getNode(Node currentNode, int value){
     if (currentNode == null)
       return null;
@@ -34,6 +56,12 @@ public class Tree {
     return getNode(currentNode.getRightNode(), value);
   }
 
+  /**
+   * Insert a new node into this tree
+   *
+   * @param currentNode Current node we are comparing to newNode
+   * @param newNode     New node that is to be inserted
+   */
   private void insertNode(Node currentNode, Node newNode) {
     if (newNode.getValue() < currentNode.getValue()) {
       if (currentNode.getLeftNode() == null)
@@ -49,6 +77,9 @@ public class Tree {
     }
   }
 
+  /**
+   * @return String representation of this Tree
+   */
   @Override
     public String toString(){
       if (rootNode != null){
